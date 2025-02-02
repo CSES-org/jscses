c0ea7c69ab2a66275c35863ac34115d36d1de44a
pylxu <e-pylxu@outlook.com>
2025-02-02 22:43:59 +0800

    修复 index.js 中的条件判断以确保 require 存在；将 package.json 版本号更新至 1.0.3

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ class CSESGenerator {
 }
 
 // 示例用法
-if (require.main && require.main === module) {
+if (require && require.main === module) {
     const args = process.argv.slice(2);
     
     if (args.length !== 1) {
